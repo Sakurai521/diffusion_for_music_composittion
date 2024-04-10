d0270da33ad21bca6bc42799f71c89dab9ad8d7b
Sakurai <sakurai.koki@em.ci.ritsumei.ac.jp>
2024-04-10 20:39:53 +0900

    Delete pretrain2mymodel.py

--- train/pretrain2mymodel.py
+++
@@ -1,9 +0,0 @@
-import torch
-import numpy as np
-
-original_path = "pretrained/polyfusion/weights_best.pt"
-original = torch.load(original_path, map_location={'cuda:0': 'cpu'})
-
-path = "results/folksong_16note_8bar_polyffusion/2023-11-23/run_6/model/model_1.npy"
-params = np.load(path, allow_pickle=True).item()
-print(original["model"].keys())
